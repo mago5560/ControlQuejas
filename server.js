afa4b7c005183faec9b49f67f6da467e7b3965ca
Luis A. Mendoza <49209006+mago5560@users.noreply.github.com>
2021-09-24 17:19:15 -0600

    chang server

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/Quejas'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Quejas/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/Quejas'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/Quejas/index.html'));});
 app.listen(process.env.PORT || 8080);
