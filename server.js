a7565fadef2a70457562364bf5108f9980ced681
Luis A. Mendoza <lmendoza@sandiego.com.gt>
2021-09-24 15:43:12 -0600

    change serve

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ControlQuejas'));
+app.use(express.static('./dist/quejas'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ControlQuejas/'}),
+    res.sendFile('index.html', {root: 'dist/quejas/'}),
 );
 
 // Start the app by listening on the default Heroku port
