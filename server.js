86fa088e3c4d5c95f7d6de48fac5b673735f533b
Luis A. Mendoza <49209006+mago5560@users.noreply.github.com>
2021-09-24 16:54:56 -0600

    next change init

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/quejas'));
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/quejas/index.html'));
-});
+app.use(express.static(__dirname + '/dist/quejas'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/quejas/index.html'));});
 ngApp.listen(process.env.PORT || 8080);
