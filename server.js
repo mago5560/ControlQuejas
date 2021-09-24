7379e70327cb9a2fd0bb851a7315af8062d968ce
Luis A. Mendoza <lmendoza@sandiego.com.gt>
2021-09-24 16:13:06 -0600

    init nose

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/quejas'));
+app.use(express.static(__dirname + '/dist/quejas'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/quejas/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/quejas/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
