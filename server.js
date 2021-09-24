5aa6ac8c8c44bb8c7fa557e060b14d3ad97f52d2
Luis A. Mendoza <49209006+mago5560@users.noreply.github.com>
2021-09-24 17:02:58 -0600

    vamos esta es la fija

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const ngApp = express();
+const app = express();
 app.use(express.static(__dirname + '/dist/quejas'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/quejas/index.html'));});
-ngApp.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
