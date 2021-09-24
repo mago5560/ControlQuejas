4192bb7ae4a4eb55064a7e4005c869b6c6f6f7c0
Luis A. Mendoza <49209006+mago5560@users.noreply.github.com>
2021-09-24 17:05:21 -0600

    dist

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/quejas'));
+app.use(express.static('./dist/quejas'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/quejas/index.html'));});
