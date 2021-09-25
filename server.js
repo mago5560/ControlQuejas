6e73d531e2d1c0eb37f0538c98f0d6449bebb014
Luis A. Mendoza <49209006+mago5560@users.noreply.github.com>
2021-09-24 18:34:11 -0600

    commit

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/Quejas'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/quejas/'}),
+app.get('*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/Quejas/'}),
 );
 
 // Start the app by listening on the default Heroku port
